46b7ab2de3ffdbe8e31ae4d5a036e12763734705
Vinicius Conti <vinicius.sconti@eaportal.org>
2025-11-03 17:29:05 -0300

    segundo commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,19 @@
+export default function Login() {
+    return (
+        <main>
+            <form action="">
+                <label htmlFor="email">Email</label>
+                <input 
+                type="text"
+                id="email"
+                />
+
+                <label htmlFor="senha">Senha</label>
+                <input 
+                type="password"
+                id="senha"
+                />
+            </form>
+        </main>
+    )
+}
