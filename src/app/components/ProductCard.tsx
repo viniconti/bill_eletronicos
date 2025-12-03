0ed385219f4df7d85ae64fb781e18964df8fe934
Vinicius Conti <vinicius.sconti@eaportal.org>
2025-12-02 22:12:39 -0300

    Commit final3

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,18 +1,22 @@
 import styles from "./ProductCard.module.css";
 
-type ProductProps = {
+interface ProductCardProps {
   name: string;
   price: string;
-};
+  image: string;
+}
 
-export default function ProductCard({ name, price }: ProductProps) {
+export default function ProductCard({ name, price, image }: ProductCardProps) {
   return (
     <div className={styles.card}>
-      <div className={styles.imagePlaceholder} />
+      <div className={styles.imagePlaceholder}>
+        <img src={image} alt={name} className={styles.image} />
+      </div>
 
       <div className={styles.cardContent}>
         <h2 className={styles.productName}>{name}</h2>
         <p className={styles.price}>{price}</p>
+
         <button className={styles.addButton}>
           Adicionar ao carrinho
         </button>
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -5,14 +5,14 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 const products = [
-  { name: "MacBook Pro", price: "R$ 14.999" },
-  { name: "iPhone 15", price: "R$ 7.499" },
-  { name: "Samsung Galaxy S24", price: "R$ 6.999" },
-  { name: "Headphone Bluetooth", price: "R$ 899" },
-  { name: 'Smart TV 55"', price: "R$ 3.499" },
-  { name: "Iphone 17 Pro", price: "R$ 11.499" },
-  { name: "Apple Watch", price: "R$ 5.499" },
-  { name: "Acer Nitro V", price: "R$ 4.699" },
+  { name: "MacBook Pro", price: "R$ 19.999", image: "/products/macbook.png"},
+  { name: "iPhone 15", price: "R$ 7.499", image: "/products/iphone15.png" },
+  { name: "Samsung Galaxy S24", price: "R$ 6.999", image: "/products/galaxys24.png" },
+  { name: "Headphone Bluetooth", price: "R$ 899", image: "/products/headphone.png" },
+  { name: 'Mouse Logitec G', price: "R$ 699", image: "/products/mouse.png" },
+  { name: "Iphone 17 Pro", price: "R$ 11.499", image: "/products/iphone17pro.png" },
+  { name: "Apple Watch", price: "R$ 5.499", image: "/products/applewatch.png" },
+  { name: "Acer Nitro V", price: "R$ 4.699", image: "/products/acernitro.png" },
 ];
 
 export default function Shop() {
@@ -29,10 +29,11 @@ export default function Shop() {
       <main className={styles.grid}>
         {products.map((product) => (
           <ProductCard
-            key={product.name}
-            name={product.name}
-            price={product.price}
-          />
+          key={product.name}
+          name={product.name}
+          price={product.price}
+          image={product.image}
+        />        
         ))}
       </main>
     </div>
